aec3b4d2d8739e4906c5556366e7119c4763351e
Namritha Kumar <namrithar88@gmail.com>
2020-08-28 19:04:05 +0530

    Added code for calendar. Also logs date in console

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+
 import { AppComponent } from './app.component';
 import { UserDietTypeComponent } from './user_diet_type/user_diet_type.component';
+import { DeliveryDateSelectorComponent } from './delivery-date-selector/delivery-date-selector.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserDietTypeComponent
+    UserDietTypeComponent,
+    DeliveryDateSelectorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory,
+    })    
   ],
   providers: [],
   bootstrap: [AppComponent]
