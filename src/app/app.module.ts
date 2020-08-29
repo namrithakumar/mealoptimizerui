029f77b027a0f33e44c6863133de3a915dddf493
Namritha Kumar <namrithar88@gmail.com>
2020-08-29 12:47:09 +0530

    Setting up all components - structure only

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,36 @@ import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 
 import { AppComponent } from './app.component';
-import { UserDietTypeComponent } from './user_diet_type/user_diet_type.component';
-import { DeliveryDateSelectorComponent } from './delivery-date-selector/delivery-date-selector.component';
+import { HeaderComponent } from './header/header.component';
+import { UserDietTypeComponent } from './meal-optimizer/user_diet_type/user_diet_type.component';
+import { DeliveryDateSelectorComponent } from './meal-optimizer/delivery-date-selector/delivery-date-selector.component';
+import { MealOptimizerComponent } from './meal-optimizer/meal-optimizer.component';
+import { MealSelectorComponent } from './meal-optimizer/meal-selector/meal-selector.component';
+import { OptimizedMealPlanComponent } from './meal-optimizer/optimized-meal-plan/optimized-meal-plan.component';
+import { OrderSummaryComponent } from './meal-optimizer/optimizer/optimized-meal-plan/order-summary/order-summary.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipes/recipe-item/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
     UserDietTypeComponent,
-    DeliveryDateSelectorComponent
+    DeliveryDateSelectorComponent,
+    MealOptimizerComponent,
+    MealSelectorComponent,
+    OptimizedMealPlanComponent,
+    OrderSummaryComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/meal-optimizer/optimized-meal-plan/optimized-meal-plan.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-optimized-meal-plan',
+  templateUrl: './optimized-meal-plan.component.html',
+  styleUrls: ['./optimized-meal-plan.component.css']
+})
+export class OptimizedMealPlanComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
