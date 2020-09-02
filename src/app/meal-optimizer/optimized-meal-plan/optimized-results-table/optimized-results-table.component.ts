abcd034dda6be33295aa0bcdd356b756f7107e2e
Namritha Kumar <namrithar88@gmail.com>
2020-09-02 15:07:40 +0530

    Adding add, delete, clear functionality to the app

--- src/app/meal-optimizer/optimized-meal-plan/optimized-results-table/optimized-results-table.component.ts
+++ src/app/meal-optimizer/optimized-meal-plan/optimized-results-table/optimized-results-table.component.ts
@@ -8,8 +8,6 @@ import { Meal } from '../../../meal-optimizer/meal.model';
 })
 export class OptimizedResultsTableComponent implements OnInit {
 
-  @ViewChild("tabTitle", { static: true }) public tabTitle : ElementRef;
-
   mealListByCost : Meal[] = [new Meal("Green Salad", 1.0, 10.0) , new Meal('Icecream',1.0,3.2), new Meal('Strawberry Milkshake', 2.0, 8.38), new Meal('Garlic bread', 1.0,4.75)];
 
   mealListByQuality : Meal[] = [new Meal("Green Salad", 1.0, 10.0) , new Meal('Icecream',1.0,3.2), new Meal('Strawberry Milkshake', 2.0, 8.38), new Meal('Garlic bread', 1.0,4.75)];
--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, Output, EventEmitter, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingEditComponent implements OnInit {
 
+  @ViewChild('ingredientName') ingredientName : ElementRef;
+  @ViewChild('ingredientAmount') ingredientAmount : ElementRef;
+
+  ingredientLabel : String = 'Added by user';
+
+  @Output() addIngredient = new EventEmitter<{ ingredientName:String, ingredientAmount:number, ingredientLabel:String }>();
+
+  @Output() deleteIngredient = new EventEmitter<{ ingredientName:String }>();
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onAddIngredient() : void {
+    this.addIngredient.emit({ ingredientName:this.ingredientName.nativeElement.value, ingredientAmount:this.ingredientAmount.nativeElement.value, ingredientLabel:this.ingredientLabel });
+  }
+
+  onDeleteIngredient() : void {
+    this.deleteIngredient.emit({ ingredientName:this.ingredientName.nativeElement.value });
+  }
+
+  onClearIngredient() : void {
+    this.ingredientName.nativeElement.value = '';
+    this.ingredientAmount.nativeElement.value = '';
+  }
 }
