a721f2d2ee531f5b09ad85e402043f5b83077647
Namritha Kumar <namrithar88@gmail.com>
2020-12-28 15:33:48 +0530

    Added a few more changes. Request method and url gets printed. Error in rendering html

--- src/offline-service-worker.js
+++ src/offline-service-worker.js
@@ -1,12 +1,12 @@
 self.addEventListener('fetch', (fetchEvent) => {
   return fetchEvent.respondWith(fetch(fetchEvent.request)
             .then(res => { return res; })
-            .catch(() => { 
-              if(caches.match('/offline.html')) {
+            .catch(() => {
+                // Check if the user is offline first and is trying to navigate to a web page
+                console.log('Request method : ' + fetchEvent.request.method);
+                console.log('Request method : ' + fetchEvent.request.url);
+                console.log('Match found in cache for /offline.html : ' + caches.match('/offline.html')?true:false);
                 return caches.match('/offline.html');
-              }
-            }))
-  
-});
-
+          }))
+        });
 importScripts('./ngsw-worker.js');
