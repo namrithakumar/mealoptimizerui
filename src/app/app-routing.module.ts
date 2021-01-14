a18b75d03903a41a8b1cfa5948a7c67fdd73f239
Namritha Kumar <namrithar88@gmail.com>
2021-01-14 09:25:43 +0530

    Added error page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,12 @@
-import { Routes } from '@angular/router';
+import { Routes, UrlSerializer } from '@angular/router';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { ContactUsComponent } from './app-info/contact-us/contact-us.component';
-import { HomeComponent } from './app-info/home/home.component';
 import { ErrorPageComponent } from '../app/shared/error-page/error-page.component';
 
 const appRoutes : Routes = [
     {path: '', redirectTo:'/app-info/home', pathMatch: 'full'},
     {path: 'error', component: ErrorPageComponent, data: {errorMessage: 'oops, something went wrong! Let us try again ...'}},
-    //{path: '**', redirectTo: '/error'}
+    {path: '**', redirectTo: '/error'}
 ];
 
 @NgModule({
