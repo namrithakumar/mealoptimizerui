d640b77f143c5595ddac11d54f0ed1b9b865626d
Namritha Kumar <namrithar88@gmail.com>
2021-01-22 11:29:30 +0530

    Added changes to display notification on connection loss/connection fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { ConnectionService } from 'ng-connection-service';
 
-import { ConnectionStatusProviderService } from './shared/services/connection-status-provider.service';
 import { AppState } from './store/reducers/app.reducer';
 import * as UserMgmtActions from './user-mgmt/store/actions/user-mgmt.actions';
 
@@ -11,17 +9,9 @@ import * as UserMgmtActions from './user-mgmt/store/actions/user-mgmt.actions';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit { 
+export class AppComponent implements OnInit {
  
-  isConnected : boolean = this.connectionStatusProviderService.getConnectionStatus();
- 
-  constructor(private store : Store<AppState>, 
-              private connectionStatusProviderService : ConnectionStatusProviderService, 
-              private connectionService : ConnectionService) {
-    this.connectionService.monitor().subscribe((connectionStatus : boolean) => {
-      this.isConnected = connectionStatus;
-  });
-  }
+  constructor(private store : Store<AppState>) { }
 
   ngOnInit() {
     this.store.dispatch(new UserMgmtActions.AutoLogin());
---
+++ src/app/shared/services/connection-status-handler.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { ConnectionService } from 'ng-connection-service';
+
+import { ConnectionLossErrorHandler } from '../services/error-handler/connection-loss-error-handler.service';
+import { NotificationDisplayService } from './notification-display.service';
+
+@Injectable({ providedIn : 'root' })
+export class ConnectionStatusHandlerService {
+ 
+    private isConnected : boolean = navigator.onLine;
+ 
+    constructor(private connectionService : ConnectionService,
+                private connectionLossErrorHandler : ConnectionLossErrorHandler,
+                private notificationDisplayService : NotificationDisplayService) {
+        this.connectionService.monitor().subscribe((connectionStatus : boolean) => {
+            this.isConnected = connectionStatus;
+            if(!connectionStatus) { this.handleConnectionLoss(); }
+            if(connectionStatus) { this.handleConnectionFix(); }
+        });  
+    }
+
+    getConnectionStatus() : boolean {
+        return this.isConnected;
+    }
+
+    handleConnectionLoss() : void {
+        this.connectionLossErrorHandler.handleError();
+    }
+
+    handleConnectionFix() {
+        //Write code to read failed requests from indexedDB and call backend.
+        this.notificationDisplayService.showNotification('Connection fixed. We will try to process your requests');
+    }
+}
