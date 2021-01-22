2e3acd31c9c87b4c94949a761e21dfc8f4fb1e2f
Namritha Kumar <namrithar88@gmail.com>
2021-01-22 22:58:11 +0530

    Added dispatch actions for login and register

--- src/app/shared/services/action-dispatcher.service.ts
+++ src/app/shared/services/action-dispatcher.service.ts
@@ -1,17 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { CalendarWeekViewCurrentTimeMarkerComponent } from 'angular-calendar/modules/week/calendar-week-view-current-time-marker.component';
 
 import { AppState } from 'src/app/store/reducers/app.reducer';
 import * as MenuActions from '../../meal-planner/meal-optimizer/store/actions/menu.actions';
 import * as OrderActions from '../../meal-planner/meal-optimizer/store/actions/order.actions';
 import * as RecipeActions from '../../meal-planner/recipes/store/actions/recipes.actions';
+import * as UserMgmtActions from '../../user-mgmt/store/actions/user-mgmt.actions';
 
 @Injectable({ providedIn : 'root' })
 export class ActionDispatcher {
 
     constructor(private store : Store<AppState>) {}
 
-    public dispatchAction(request : any, tag : String) {
+    public dispatchAction(request : any, tag : String) : void {
                 
         //Logic to dispatch action based on tag
         switch(tag) {
@@ -30,6 +32,17 @@ export class ActionDispatcher {
                                     this.store.dispatch(new RecipeActions.FetchRecipesStart(itemNames));
                                     break;
                                  }
+            case 'user-login' : {
+                                    let loginRequest = request.body;
+                                    this.store.dispatch(new UserMgmtActions.LoginStart(loginRequest));
+                                    break;
+                                }                     
+            case 'user-register' : {
+                                    let signupReq = request.body;
+                                    this.store.dispatch(new UserMgmtActions.SignupStart(signupReq));
+                                    break;
+                                    }                   
         }
+        return;
     }    
 }
