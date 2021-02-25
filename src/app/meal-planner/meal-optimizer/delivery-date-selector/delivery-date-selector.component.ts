c3eca0898dcdc83c40a4ff92a542ea7fe84651f4
Namritha Kumar <namrithar88@gmail.com>
2021-02-25 20:23:50 +0530

    Fixed button sizes - recipes > recipe list > recipe item

--- src/app/meal-planner/meal-optimizer/delivery-date-selector/delivery-date-selector.component.ts
+++ src/app/meal-planner/meal-optimizer/delivery-date-selector/delivery-date-selector.component.ts
@@ -2,13 +2,11 @@ import {
     Component,
     ElementRef,
     OnInit,
-    ViewChild,
-    Renderer2
+    ViewChild
   } from '@angular/core';
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import { Calendar } from "@fullcalendar/core";
-import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
 
 import { AppState } from 'src/app/store/reducers/app.reducer';
 import { Store } from '@ngrx/store';
@@ -29,9 +27,7 @@ import * as UserPreferencesActions from '../store/actions/user-preferences.actio
 
     propertyName = 'deliveryDate';
 
-    constructor(private store : Store<AppState>,
-                private breakpointObserver: BreakpointObserver,
-                private renderer: Renderer2) { }
+    constructor(private store : Store<AppState>) { }
    
     ngOnInit() { }
 
@@ -49,28 +45,7 @@ import * as UserPreferencesActions from '../store/actions/user-preferences.actio
         aspectRatio: 2.4,
         dateClick: this.handleDateClick.bind(this)
       });
-      
+
       this.calendar.render();
-      //Track the size of the window and set aspectRatio (width:height) ratio of the calendar.
-      //AspectRatio is a property of fullcalendar ver 4.
-      //Note that angular cdk screen sizes(small, medium, large etc.) are different from bootstrap sizes.
-      /*this.breakpointObserver.observe([
-        Breakpoints.XSmall,
-        Breakpoints.Small,
-        Breakpoints.Medium,
-        Breakpoints.Large,
-        Breakpoints.XLarge
-      ]).subscribe( (state: BreakpointState) => {
-        if (state.breakpoints[Breakpoints.XSmall] || 
-            state.breakpoints[Breakpoints.Small]) {
-             this.calendar.setOption('aspectRatio', 1.35);
-        }
-        else if (state.breakpoints[Breakpoints.Medium] || 
-                 state.breakpoints[Breakpoints.Large] || 
-                 state.breakpoints[Breakpoints.XLarge]) {
-            this.calendar.setOption('aspectRatio', 2.4);
-        }
-        this.calendar.render();
-      });*/
     }
   }
