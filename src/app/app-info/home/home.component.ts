932b0791566c19ef96f1fd3378e74193d2928e01
Namritha Kumar <namrithar88@gmail.com>
2021-03-18 22:42:21 +0530

    Modified home screen to display correct button based on login status of user

--- src/app/app-info/home/home.component.ts
+++ src/app/app-info/home/home.component.ts
@@ -15,7 +15,8 @@ export class HomeComponent implements OnInit {
 
   authenticatedUser : User;
 
-  constructor(private router:Router, private store : Store<AppState>) { }
+  constructor(private router:Router, 
+              private store : Store<AppState>) { }
 
   ngOnInit(): void {
     this.store.select('authenticatedUser').subscribe(( authenticatedUser : AuthenticatedUser ) => {
@@ -23,10 +24,17 @@ export class HomeComponent implements OnInit {
     });
   }
 
+  //Displayed if user has not logged in
   goToLoginOrRegister() : void {
-    //this.router.navigate(['/user-mgmt','login']);
+    this.router.navigate(['/meal-planner', { outlets: { mealoptimizer : ['meal-optimizer'] } }], { queryParams: {optimizermode: 'create'} });
   }
 
+  //Displayed if user has not logged in
+  continueAsGuest() : void {
+    console.log('Code to be written to continue as unauthenticated guest'); 
+  }
+
+  //Displayed if user has logged in
   goToMealPlanner() : void {
     this.router.navigate(['/meal-planner', { outlets: { mealoptimizer : ['meal-optimizer'] } }], { queryParams: {optimizermode: 'create'} });
   }
