fd5959cb62d137604e5e8e4e36e9c29ae4917d86
Namritha Kumar <namrithar88@gmail.com>
2021-06-10 21:56:22 +0530

    Usign alternative port 8080 for http requests as original port 80 is used by mealplanner

--- src/app/shared/app.init.ts
+++ src/app/shared/app.init.ts
@@ -4,7 +4,7 @@ export function initializeKeycloak(keycloak: KeycloakService) {
     return () =>
       keycloak.init({
         config: {
-          url: 'http://avocadoandeggsclub-iam.xyz/auth',
+          url: 'http://avocadoandeggsclub-iam.xyz:8080/auth',
           realm: 'mealplanner',
           clientId: 'meal-planner-app-client'
         },
