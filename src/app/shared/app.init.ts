8472edbceb1be484d08587b22e33b6cefd1f12f9
Namritha Kumar <namrithar88@gmail.com>
2021-06-10 21:47:58 +0530

    Modified Keycloak url to avocadoandeggsclub-iam.xyz

--- src/app/shared/app.init.ts
+++ src/app/shared/app.init.ts
@@ -4,7 +4,7 @@ export function initializeKeycloak(keycloak: KeycloakService) {
     return () =>
       keycloak.init({
         config: {
-          url: 'http://meal-optimizer-iam:8080/auth',
+          url: 'http://avocadoandeggsclub-iam.xyz/auth',
           realm: 'mealplanner',
           clientId: 'meal-planner-app-client'
         },
