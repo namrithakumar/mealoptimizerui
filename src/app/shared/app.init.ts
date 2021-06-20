45a1edc0769b196505b38f2ab431b2b4a1fcc1db
Namritha Kumar <namrithar88@gmail.com>
2021-06-21 01:13:06 +0530

    Modified Keycloak URL, also fixed the recipe start text

--- src/app/shared/app.init.ts
+++ src/app/shared/app.init.ts
@@ -4,8 +4,8 @@ export function initializeKeycloak(keycloak: KeycloakService) {
     return () =>
       keycloak.init({
         config: {
-          //url: 'http://www.myclaypot-iam.xyz/auth',
-          url: 'http://www.myclaypot-iam.xyz:8080/auth',
+          url: 'http://www.myclaypot-iam.xyz/auth',
+          //url: 'http://www.myclaypot-iam.xyz:8080/auth',
           realm: 'mealplanner',
           clientId: 'meal-planner-app-client'
         },
