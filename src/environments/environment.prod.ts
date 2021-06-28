d436eea75b5f45dc699c013875e688d8a521cc78
Namritha Kumar <namrithar88@gmail.com>
2021-06-29 01:47:53 +0530

    Added PROD config, reduced image size using imagify

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  hostUrl : window.location.protocol + '//' + window.location.host,
+  apiUrl: window.location.protocol + '//' + window.location.host,
+  apiPort: 80,
+  applicationName : 'mealoptimizer',
+  envName : 'prod'
 };
